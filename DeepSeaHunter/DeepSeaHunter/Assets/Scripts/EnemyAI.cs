a5a410b2a0a7be64f63664f8d5a4b1a21a5875f6
SpaceDemonMars <inkapaige@gmail.com>
2025-03-10 23:13:05 -0700

    Revert "fixed crash"
    This reverts commit c06138a3f5b5f4e22d0501b177e59070b75be2de.

--- DeepSeaHunter/DeepSeaHunter/Assets/Scripts/EnemyAI.cs
+++ DeepSeaHunter/DeepSeaHunter/Assets/Scripts/EnemyAI.cs
@@ -4,62 +4,49 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour, IDamage
 {
-    [SerializeField] int HP;
     [SerializeField] Renderer model;
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Animator anim;
 
-    [SerializeField] int faceTargetSpeed;
-    [SerializeField] int animTranSpeed;
-
     Color modelColor;
     Vector3 playerDir;
-    bool playerInRange;
 
+    bool playerInRange;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        GameManager.instance.updateGameGoal(1);
+        modelColor = model.material.color;
     }
+
     // Update is called once per frame
     void Update()
     {
-        setAnimLocomotion();
+        
+    }
 
-        if (playerInRange)
+    public void takeDamage(int damage)
+    {
+        HP -= damage;
+        StartCoroutine(flashRed());
+
+        if (HP <= 0)
         {
-            playerDir = GameManager.instance.player.transform.position - transform.position;
-            agent.SetDestination(GameManager.instance.player.transform.position);
-
-            if (agent.remainingDistance <= agent.stoppingDistance)
-            {
-                faceTarget();
-            }
+            GameManager .instance.updateGameGoal(-1);
+            Destroy(gameObject);
         }
     }
-    void setAnimLocomotion()
-    {
-        float agentSpeed = (agent.velocity.normalized.magnitude);
-        float animSpeedCur = anim.GetFloat("Speed");
-        anim.SetFloat("Speed", Mathf.Lerp(animSpeedCur, agentSpeed, Time.deltaTime * animTranSpeed));
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = true;
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = false;
-        }
-    }
-    void faceTarget()
-    {
-        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
-        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
 
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(.1f);
+        model.material.color = modelColor;
     }
+
+    void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, shootPos.position, transform.rotation);
+    }
+}
