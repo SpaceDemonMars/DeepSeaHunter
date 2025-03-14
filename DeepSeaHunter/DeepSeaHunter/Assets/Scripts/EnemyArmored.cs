2261885d4435baa37d5e4d0f400cb6d67f942ffe
SpaceDemonMars <inkapaige@gmail.com>
2025-03-14 02:37:43 -0700

    Crabs!

--- DeepSeaHunter/DeepSeaHunter/Assets/Scripts/EnemyArmored.cs
+++ DeepSeaHunter/DeepSeaHunter/Assets/Scripts/EnemyArmored.cs
@@ -1,37 +1,114 @@
-using Unity.VisualScripting;
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyArmored : MonoBehaviour, IDamage
 {
-    [SerializeField] EnemyAI parent;
-    [SerializeField] Material flashDamage;
-    [SerializeField] Material damaged;
     [SerializeField] int HP;
+    [SerializeField] Animator anim;
+    public Renderer model;
+    public Material flashDamage;
 
-    Material parentMaterial;
+    public NavMeshAgent agent;
 
+    [SerializeField] int numBullets;
+    [SerializeField] float timeBetweenShots;
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform shootPos;
+
+    [SerializeField] float shootRate;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] int animTranSpeed;
+
+    float shootTimer;
+
+    Vector3 playerDir;
+
+    bool playerInRange;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        parentMaterial = parent.model.material;
+        GameManager.instance.updateGameGoal(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //setAnimLocomotion();
+        shootTimer += Time.deltaTime;
+        if (playerInRange)
+        {
+            playerDir = GameManager.instance.player.transform.position - transform.position;
+            agent.SetDestination(GameManager.instance.player.transform.position);
+
+            if (shootTimer >= shootRate)
+                shoot();
+            if (agent.remainingDistance <= agent.stoppingDistance)
+                faceTarget();
+        }
+    }
+
+    void setAnimLocomotion()
+    {
+        float agentSpeed = agent.velocity.normalized.magnitude;
+        float animSpeed = anim.GetFloat("Speed");
+        anim.SetFloat("Speed", Mathf.Lerp(animSpeed, agentSpeed, Time.deltaTime * animTranSpeed));
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
     }
 
     public void takeDamage(int damage)
     {
         HP -= damage;
-        StartCoroutine(flashWhite());
+        StartCoroutine(flashRed());
+        agent.SetDestination(GameManager.instance.player.transform.position);
 
         if (HP <= 0)
         {
-            parent.model.material = damaged;
+            GameManager.instance.updateGameGoal(-1);
             Destroy(gameObject);
         }
     }
 
-    IEnumerator flashWhite()
+    IEnumerator flashRed()
     {
-        parent.model.material = flashDamage;
+        Material mat = model.material;
+        model.material = flashDamage;
         yield return new WaitForSeconds(.1f);
-        parent.model.material = parentMaterial;
+        model.material = mat;
+    }
+
+    void shoot()
+    {
+        StartCoroutine(shootMultiple());
+        shootTimer = 0;
+    }
+
+    IEnumerator shootMultiple()
+    {
+        for (int i = 0; i < numBullets; i++)
+        {
+            Instantiate(bullet, shootPos.position, transform.rotation);
+            yield return new WaitForSeconds(timeBetweenShots);
+        }
     }
 }
