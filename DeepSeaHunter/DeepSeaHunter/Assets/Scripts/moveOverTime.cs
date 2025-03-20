54b40f192030e1acb81ccb1ecb2de797302da839
SpaceDemonMars <inkapaige@gmail.com>
2025-03-20 01:48:58 -0700

    Seafoam and Boat Timeline

--- DeepSeaHunter/DeepSeaHunter/Assets/Scripts/moveOverTime.cs
+++ DeepSeaHunter/DeepSeaHunter/Assets/Scripts/moveOverTime.cs
@@ -3,13 +3,38 @@ using UnityEngine;
 public class moveOverTime : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
+    [SerializeField] float rotSpeed;
     [SerializeField] Transform startPos;
     [SerializeField] Transform endPos;
     // Update is called once per frame
+
+    [SerializeField] bool moveAngle;
+    [SerializeField] bool moveAngleFast;
+    Transform target;
+
+    private void Start()
+    {
+        target = endPos;
+    }
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, endPos.position, moveSpeed * Time.deltaTime);
         if (transform.position == endPos.position)
             transform.position = startPos.position;
+
+        if (moveAngle)
+        {
+            if (moveAngleFast)
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, rotSpeed * Time.deltaTime);
+            else
+                transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, rotSpeed * Time.deltaTime);
+            if (transform.rotation == target.rotation)
+            {
+                if (target == endPos)
+                    target = startPos;
+                else
+                    target = endPos;
+            }
+        }
     }
 }
