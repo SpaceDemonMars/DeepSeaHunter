6ecdd48e0c91a1ea8b25a32172de4ea0afab6209
Leanne McGovern <leannemcgovern@hotmail.com>
2025-03-20 04:04:32 -0400

    reorganizing

--- DeepSeaHunter/DeepSeaHunter/Assets/Scripts/playerController.cs
+++ DeepSeaHunter/DeepSeaHunter/Assets/Scripts/playerController.cs
@@ -50,6 +50,8 @@ public class playerController : MonoBehaviour, IDamage, ITangle, IHarpoon, IPick
     float shootTimer;
     public float shootDist;
     public bool isTangled;
+    private float harpoonChargeSpeed;
+    private float harpoonPullSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
