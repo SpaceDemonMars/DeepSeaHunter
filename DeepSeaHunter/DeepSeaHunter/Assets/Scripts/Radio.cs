6456218b7782d75e203270b9e647faee2e4cc734
SpaceDemonMars <inkapaige@gmail.com>
2025-04-17 23:39:22 -0700

    Radio

--- DeepSeaHunter/DeepSeaHunter/Assets/Scripts/Radio.cs
+++ DeepSeaHunter/DeepSeaHunter/Assets/Scripts/Radio.cs
@@ -6,11 +6,14 @@ public class Radio : MonoBehaviour
     [Range(0, 1)][SerializeField] float radioVol; //WHEN SETTINGS MADE, REMOVE SERIALIZE FIELD, FINISH SETVOLUME FUNC
     [SerializeField] AudioClip[] radioPlaylist;
     [SerializeField] AudioClip[] staticPlaylist;
+    [SerializeField] AudioClip[] onOffClick;
+    [Range(0, 1)][SerializeField] float clickVol;
     AudioClip[][] allRadioClips;
     int[] playlistIndex = { 0, 0 };
 
-    [SerializeField] bool radioOn;
-    public bool inStatic;
+    bool radioOn;
+    [SerializeField] bool inStatic; //in a zone with static
+
     void Start()
     {
         allRadioClips = new AudioClip[2][];
@@ -23,6 +26,30 @@ public class Radio : MonoBehaviour
     void Update()
     {
         toggleRadio();
+        playRadio();
+    }
+
+    void playRadio()
+    {
+        if (!aud.isPlaying) //if not playing (current clip ended)
+        { 
+            aud.clip = getNextClip();
+            aud.Play();
+        }
+    }
+
+    AudioClip getNextClip()
+    {
+        int playlist = (inStatic) ? 1 : 0;
+        if ((inStatic && staticPlaylist.Length > 0) || //playing static && playlist populated
+            (!inStatic && radioPlaylist.Length > 0)) //or playing radio && playlist populated
+        {
+            //if current index is last for current playlist ==> reset
+            if (playlistIndex[playlist] >= allRadioClips[playlist].Length - 1) playlistIndex[playlist] = 0; 
+            else playlistIndex[playlist]++; //else increment index
+            return allRadioClips[playlist][playlistIndex[playlist]]; //returns clip at [currPlaylist][index]
+        }
+        else return null;
     }
 
     void toggleRadio()
@@ -30,6 +57,7 @@ public class Radio : MonoBehaviour
         if (Input.GetButtonDown("Radio"))
         {
             radioOn = !radioOn;
+            aud.PlayOneShot(onOffClick[Random.Range(0, onOffClick.Length)], clickVol);
             SetRadioVol();
         }
     }
@@ -40,5 +68,13 @@ public class Radio : MonoBehaviour
         if (radioOn)
             aud.volume = radioVol; 
         else aud.volume = 0;
+    }
+
+    bool getInStatic() { return inStatic; }
+    public void setInStatic(bool value) 
+    {
+        inStatic = value;
+        aud.Stop(); //stops current clip so that correct clip will play
+        playRadio(); //starts new clip 
     }
 }
