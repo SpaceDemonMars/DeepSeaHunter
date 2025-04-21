214851c3a1dff0a29410219db521cb5fc3aa6c28
Leanne McGovern <leannemcgovern@hotmail.com>
2025-04-20 22:23:11 -0400

    working on journal and clue set up and prefab of NPCs

--- DeepSeaHunter/DeepSeaHunter/Assets/Scripts/player/playerInteract.cs
+++ DeepSeaHunter/DeepSeaHunter/Assets/Scripts/player/playerInteract.cs
@@ -3,10 +3,8 @@ using TMPro;
 
 public class PlayerInteract : MonoBehaviour
 {
-    //pushing a fix and leaving you notes :3 (delete these after you've read them)
     public float interactRange = 3f;
     public LayerMask interactableLayer;
-    //public TMP_Text interactPrompt; this is a UI element, so this line gets moved to gameManager
 
     private IInteractable currentInteractable;
 
@@ -17,7 +15,7 @@ public class PlayerInteract : MonoBehaviour
         if (currentInteractable != null && Input.GetButtonDown("Interact"))
         {
             currentInteractable.Interact();
-            GameManager.instance.interactPrompt.gameObject.SetActive(false); //toggle active from gameManager
+            GameManager.instance.interactPrompt.gameObject.SetActive(false); 
         }
     }
 
